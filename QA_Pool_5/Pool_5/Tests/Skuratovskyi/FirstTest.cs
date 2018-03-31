ee34c69f94475d307acf1b908f9e9b5f554d0cdb
Olga Makarova <me.ola.makarova@gmail.com>
2018-03-31 21:44:19 +0300

    initial commit

---
+++ QA_Pool_5/Pool_5/Tests/Skuratovskyi/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool_5.Tests.Skuratovskyi
+{
+    class FirstTest
+    {
+        // test
+        // test 2
+        // test 3 
+    }
+}
