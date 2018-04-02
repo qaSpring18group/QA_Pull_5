448aca471fa3cf6593385e781b0d06ccc1a60800
dikobrazz <lanskix.mariya@gmail.com>
2018-04-02 14:24:56 +0300

    getlib in my test

--- QA_Pool_5/Pool_5/Tests/Lanskix/Start (2).cs
+++ QA_Pool_5/Pool_5/Tests/Lanskix/Start (2).cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SeleniumSupport;
+using NUnit.Framework;
+using Selenium.Chrome.WebDriver;
+
 namespace Lanskix
 {
-    class Start
+    [TestMethod]
+
+    public class Start
+
     {
+        ChromeWebDriver chrome = new ChromeWebDriver();
     }
 }
