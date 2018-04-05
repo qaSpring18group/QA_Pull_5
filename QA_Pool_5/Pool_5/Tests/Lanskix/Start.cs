33c6f54e043ae1e45d0997adabb43e2cd9161b6d
dikobrazz <lanskix.mariya@gmail.com>
2018-04-05 15:54:18 +0300

    Check

--- QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
+++ QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Open.QA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace PooL_5.tests
+namespace PooL_5
+{ 
+    [TestClass]
 
-{
-    class Start
+  public class Start
     {
+        [TestMethod]
+        public void TestStarter();
+
+                 
+             
+         }
     }
-}
+
