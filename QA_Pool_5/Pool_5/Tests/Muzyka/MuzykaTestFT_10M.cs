cfbb00d9b16e6aeefcad8597ac91dfd2f22c5fc4
Maxim <Maximmuzyka@gmail.com>
2018-04-06 01:02:00 +0300

    My test

--- QA_Pool_5/Pool_5/Tests/Muzyka/MuzykaTestFT_10M.cs
+++ QA_Pool_5/Pool_5/Tests/Muzyka/MuzykaTestFT_10M.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pool_5.Tests.myzuka
+namespace Pool_5
 {
-    class Class1
+    class MuzykaTestFT_10M
     {
     }
 }
