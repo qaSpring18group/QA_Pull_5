52a8ac23c253ed8f491c5a9a75eb4db0628e8714
dikobrazz <lanskix.mariya@gmail.com>
2018-04-05 14:35:11 +0300

    RefixLib

--- QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
+++ QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PooL_5;
+using System.IO;
 
-namespace PooL_5
+namespace PooL_5.tests
 
 {
     class Start
