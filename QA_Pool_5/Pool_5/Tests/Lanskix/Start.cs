58df28af29fb420b1f79e440d423fecda242899a
dikobrazz <lanskix.mariya@gmail.com>
2018-04-05 11:08:20 +0300

    change namespase in test

--- QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
+++ QA_Pool_5/Pool_5/Tests/Lanskix/Start.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PooL_5;
 
-namespace Lanskix
+namespace PooL_5
+
 {
     class Start
     {
